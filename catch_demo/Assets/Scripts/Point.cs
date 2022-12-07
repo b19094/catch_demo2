5ca09eddd7e478d6e4bf0e31d108b618307c947d
e1b19094 <e1b19094@oit.ac.jp>
2022-12-08 02:57:38 +0900

    fix

---
+++ catch_demo/Assets/Scripts/Point.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Point : MonoBehaviour
+{
+    public Text info;
+    public GameObject obj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick()
+    {
+        info.text = obj.GetComponent<Text>().text;
+    }
+
+
+}
